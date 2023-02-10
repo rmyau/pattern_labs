5db1b49a3ce63041eda08d4af3f495636c327146
rmyau <katya.anishenko24@gmail.com>
2023-02-10 16:16:59 +0300

    Added method 4(1.37) of task4 - array of indexes elements which less than left element

--- lab1/task4/task4.rb
+++ lab1/task4/task4.rb
@@ -12,4 +12,13 @@ end
 def max_in_interval(list, a,b)
   list[a..b].max
 end
-puts max_in_interval([1,2,-2,4],0,0)
+
+def index_less_than_left(list)
+  list.filter {|x| list.index(x)!=0 && x<list[list.index(x)-1] }.map {|el| list.index(el)}
+end
+puts index_less_than_left([2,4,1,5,3])
+
+
+
+
+# puts max_in_interval([1,2,-2,4],0,0)

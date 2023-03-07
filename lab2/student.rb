23ec64a728b10ca58bdc4324e99220adc05784d9
rmyau <katya.anishenko24@gmail.com>
2023-03-07 21:21:05 +0300

    Валидация для номера телефона(+2.1_7)

--- lab2/student.rb
+++ lab2/student.rb
@@ -1,7 +1,7 @@
 
 class Student
-  attr_accessor :id, :first_name, :second_name, :last_name, :phone, :telegram, :git, :email
-
+  attr_accessor :id, :first_name, :second_name, :last_name, :telegram, :git, :email
+  attr_reader :phone
   def initialize(last_name, first_name, second_name, id:nil, phone:nil, telegram: nil, git:nil, email: nil )
     self.last_name=last_name
     self.first_name=first_name
@@ -12,16 +12,18 @@ class Student
     self.git=git
     self.email=email
   end
-  # def initialize(**hash_args)
-  #   self.last_name=hash_args[:last_name]
-  #   self.first_name=hash_args[:first_name]
-  #   self.second_name=hash_args[:second_name]
-  #   # self.id=id
-  #   # self.phone=phone
-  #   # self.telegram=telegram
-  #   # self.git=git
-  #   # self.email=email
-  # end
+
+  #set phone
+  def phone=(phone)
+    raise ArgumentError, 'Invalid phone number!' unless Student.validate_phone?(phone)
+    @phone = phone
+  end
+
+  #проверка на номер
+  def self.validate_phone?(phone)
+    # phone.match(/^(8|\+7)[\- ]?\(?\d{3}\)?[\-_]?\d{3}([\-_]?\d{2}){2}$/)
+    phone.match(/^(8|\+7)[\- ]?\(?\d{3}\)?[\- ]?\d{3}([\- ]?\d{2}){2}$/)
+  end
 
   def to_s
     res = "#{last_name} #{first_name} #{second_name}"

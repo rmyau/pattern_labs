7ba7bc83256ed6bf887180f37d09651bc6216f47
rmyau <katya.anishenko24@gmail.com>
2023-03-18 13:20:18 +0300

    Закрыты поля контактов для доступа только через set_contact

--- lab2/student.rb
+++ lab2/student.rb
@@ -4,7 +4,7 @@ class Student<BaseStudent
   public_class_method :new
   attr_reader :first_name, :second_name, :last_name
   #открываем сеттеры и геттеры из базового класса
-  public :phone, :telegram, :email, 'id=', 'phone=', 'telegram=', 'email=', 'git=', :set_contacts
+  public :phone, :telegram, :email, 'id=',  'git=', :set_contacts
   def initialize(last_name: nil, first_name: nil, second_name: nil, id: nil, phone:nil, telegram: nil, email: nil, git:nil)
     raise ArgumentError, "Required fields: first_name, second_name and last_name!" if first_name.nil? || second_name.nil?|| last_name.nil?
     self.last_name=last_name

d72c90869ee907c2dc9a9e067905e98bbb90bca1
rmyau <katya.anishenko24@gmail.com>
2023-03-31 19:45:12 +0300

    +2.4_3 Добавлен класс StudentListYaml

--- lab2/brbr.rb
+++ lab2/brbr.rb
@@ -1,8 +1,9 @@
 require_relative 'files_model/student_list_json'
 require_relative 'files_model/student_list_txt'
+require_relative 'files_model/student_list_yaml'
 
-a = StudentListTxt.new()
-a.read_file("C:/Users/katya/Desktop/ruby_lab/lab2/student_list.txt")
+# a = StudentListTxt.new()
+# a.read_file("C:/Users/katya/Desktop/ruby_lab/lab2/student_list.txt")
 # puts a.students
 
 # puts a.get_k_n_student_short_list(1,2)
@@ -10,9 +11,18 @@ a.read_file("C:/Users/katya/Desktop/ruby_lab/lab2/student_list.txt")
 # a.sorted
 
 list=[1,2,3]
+#
+# a = StudentListJson.new()
+# a.read_file("C:/Users/katya/Desktop/ruby_lab/lab2/student_list.json")
+# # puts a.students
+# a.sorted_by_name
+# a.write_to_file("C:/Users/katya/Desktop/ruby_lab/lab2/student_list_out.json")
+#
 
-a = StudentListJson.new()
-a.read_file("C:/Users/katya/Desktop/ruby_lab/lab2/student_list.json")
+
+a = StudentListYaml.new()
+a.read_file("C:/Users/katya/Desktop/ruby_lab/lab2/test_data/student_list.yaml")
 # puts a.students
-a.sorted_by_name
-a.write_to_file("C:/Users/katya/Desktop/ruby_lab/lab2/student_list_out.json")
+# a.sorted_by_name
+a.write_to_file("C:/Users/katya/Desktop/ruby_lab/lab2/test_data/student_list_out.yaml")
+
---
+++ lab2/files_model/student_list_yaml.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require_relative 'base_student_list'
+require 'yaml'
+
+class StudentListYaml<BaseStudentList
+  public_class_method :new
+
+  private
+  #получение массива хэшей из строки
+  def list_hash_from_str(str)
+    YAML.load(str).map {|hash| hash.transform_keys(&:to_sym)}
+  end
+
+  #получение строки заданного вида из массива хэшей
+  def list_hash_to_str(list_hash)
+    list_hash.map{ |hash| hash.transform_keys(&:to_s)}.to_yaml
+  end
+
+end

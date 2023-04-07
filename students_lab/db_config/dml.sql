2e5f15efcea743d2b99114163b69f19dc13668a2
rmyau <katya.anishenko24@gmail.com>
2023-04-07 22:45:14 +0300

    +3.1_4 Заполнение таблицы

---
+++ students_lab/db_config/dml.sql
@@ -0,0 +1,4 @@
+insert into students(last_name, first_name, second_name, phone, telegram, email, git) values
+  ('Иванов', 'Иван', 'Иванович', '79183451223', NULL, 'test@mail.ru', '@ivan_proger'),
+  ('Помидоров', 'Томато', 'Томатович', NULL, NULL, '@tomato@mail.ru', NULL),
+  ('Придумович', 'Имен', 'Отчествович', '83459002387', '@namecreator', 'namer@yandex.ru', '@name_creator');

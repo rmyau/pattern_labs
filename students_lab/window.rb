3a16dca06d71d660ca0da7216b705e7cd987122c
rmyau <katya.anishenko24@gmail.com>
2023-04-14 21:24:04 +0300

    Сделана таблица!! Плакала я над этой вашей документацией

--- students_lab/window.rb
+++ students_lab/window.rb
@@ -7,8 +7,10 @@ class Window<FXMainWindow
 
     tab_book = FXTabBook.new(self, nil, 0, LAYOUT_FILL_X||LAYOUT_FILL_Y)
     # Создаем первую вкладку
-    @tab1 = FXTabItem.new(tab_book, "Вкладка 1", nil)
-    @composite1 = FXComposite.new(tab_book, LAYOUT_FILL_X||LAYOUT_FILL_Y)
+    tab1 = FXTabItem.new(tab_book, "Вкладка 1", nil)
+    composite1 = FXComposite.new(tab_book, LAYOUT_FILL_X||LAYOUT_FILL_Y)
+    @first_tab = FXHorizontalFrame.new(composite1)
+    @first_tab.resize(1000,1000)
     first_tab
 
     # Создаем вторую вкладку
@@ -21,19 +23,19 @@ class Window<FXMainWindow
   end
   def create
     super
-    @composite1.create
     show(PLACEMENT_SCREEN)
   end
 
   private
   def first_tab
     add_filters
+    add_table
 
   end
 
   def add_filters
     #filter
-    @frame_filter = FXVerticalFrame.new(@composite1)
+    @frame_filter = FXVerticalFrame.new(@first_tab)
     @frame_filter.resize(500,300)
 
     field_filter =[[:git, 'Гит'], [:email, 'Почта'], [:phone, 'Телефон'], [:telegram, 'Телеграм']]
@@ -44,10 +46,74 @@ class Window<FXMainWindow
 
     #фильтрация для остальных полей
     field_filter.each do |field|
-      @filter[field[0]] = create_radio_group(field, frame_filter)
+      @filter[field[0]] = create_radio_group(field, @frame_filter)
     end
   end
 
+  def add_table
+
+    # Создаем таблицу
+    table = FXTable.new(@first_tab, :opts =>  TABLE_READONLY|LAYOUT_FIX_WIDTH|LAYOUT_FIX_HEIGHT, :width=>580, :height=>220)
+    table.setTableSize(10, 3)
+
+    table.setColumnText(0, "ФИО")
+    table.setColumnText(1, "Git")
+    table.setColumnText(2, "Контакт")
+
+    (1..10).each { |num| table.setRowText(num-1, num.to_s) }
+
+    table.setColumnText(0, "Имя")
+    table.setColumnText(1, "Твиттер")
+    table.setColumnText(2, "Email")
+
+    # Заполняем таблицу данными
+    table.setItemText(0, 0, "John")
+    table.setItemText(0, 1, "@jo")
+    table.setItemText(0, 2, "john@example.com")
+
+    table.setItemText(1, 0, "Jane")
+    table.setItemText(1, 1, "@frfff")
+    table.setItemText(1, 2, "jane@example.com")
+
+    table.setItemText(2, 0, "Bob")
+    table.setItemText(2, 1, "@bob")
+    table.setItemText(2, 2, "bob@example.com")
+
+    table.setItemText(3, 0, "Alice")
+    table.setItemText(3, 1, "@alice")
+    table.setItemText(3, 2, "alice@example.com")
+
+    table.setItemText(4, 0, "Mike")
+    table.setItemText(4, 1, "@mike")
+    table.setItemText(4, 2, "mike@example.com")
+
+    table.setItemText(5, 0, "Sara")
+    table.setItemText(5, 1, "@sara")
+    table.setItemText(5, 2, "sara@example.com")
+
+    table.setItemText(6, 0, "Tom")
+    table.setItemText(6, 1, "@tom")
+    table.setItemText(6, 2, "tom@example.com")
+
+    table.setItemText(7, 0, "Emily")
+    table.setItemText(7, 1, "@emily")
+    table.setItemText(7, 2, "emily@example.com")
+
+    table.setItemText(8, 0, "Max")
+    table.setItemText(8, 1, "@max")
+    table.setItemText(8, 2, "max@example.com")
+
+    table.setItemText(9, 0, "Lucy")
+    table.setItemText(9, 1, "@lucy")
+    table.setItemText(9, 2, "lucy@example.com")
+
+    # Масштабируем таблицу
+    table.setRowHeaderWidth(50)
+    table.setColumnWidth(0, 150)
+    table.setColumnWidth(1, 150)
+    table.setColumnWidth(2, 200)
+
+  end
 
   def create_radio_group(field, parent)
     #Фильтрация гита
@@ -89,6 +155,7 @@ class Window<FXMainWindow
     end
     frame_field
   end
+
 end
 
 app = FXApp.new
